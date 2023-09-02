5b2d66e662dddecb0caf3b3c0f7be02bc249d7f3
Deepak Suthar <codewithdeepak@yahoo.com>
2023-09-02 18:55:43 +0530

    feat: added husky

--- app/src/components/Page.tsx
+++ app/src/components/Page.tsx
@@ -2,14 +2,11 @@ import { ReactNode } from "react";
 import DashboardLayout from "./layouts/DashboardLayout";
 
 type PageProps = {
-    children: ReactNode
-    title: string
-}
+  children: ReactNode;
+  title: string;
+};
 
-export default function Page({children,title}:PageProps) {
-    return (
-        <DashboardLayout pageTitle={title}>
-            {children}
-        </DashboardLayout>
-    )
+export default function Page({ children, title }: PageProps) {
+  console.log("This is a page");
+  return <DashboardLayout pageTitle={title}>{children}</DashboardLayout>;
 }
